1606417f4edacf381a5ae4e09f216279c28c1509
MIchael Berry <michaeladamberry3@gmail.com>
2019-02-10 14:38:55 -0500

    setting up dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = { data: null };
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch("/express_backend");
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -15,11 +33,11 @@ class App extends Component {
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Learn React
           </a>
         </header>
+        <p className="App-intro">{this.state.data}</p>
       </div>
     );
   }
